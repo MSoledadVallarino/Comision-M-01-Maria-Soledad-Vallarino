5880580ebf9932a22e02e7a49454059ca023b20a
Maria Soledad Vallarino <msv3577@gmail.com>
2023-11-29 18:39:00 -0300

    feat: se crearon los modelos para post, comment y user

---
+++ Backend/src/models/Comment.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+
+const CommentSchema = new Schema({
+  autor: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  description: {
+    type: String,
+    maxLength: 1000,
+    minLength: 100,
+  },
+  timestamps: true,
+  versionKey: false,
+});
--- Backend/src/models/Post.js
+++ Backend/src/models/Post.js
@@ -1,1 +1,35 @@
-export const PostModel = {};
+import { Schema, model } from "mongoose";
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 200,
+    minLength: 5,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 5000,
+    minLength: 100,
+  },
+  autor: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  comments: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment",
+  },
+
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  timestamps: true,
+  versionKey: false,
+});
+
+export const PostModel = model("Post", PostSchema);
